726d89ea8eb94b1cfad36ce213b63c5b4603e28c
Abdoulaye DIALLO <abdoulaye.diallo@banoconsulting.com>
2025-04-30 17:06:14 +0200

    models

--- my_app/admin.py
+++ my_app/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
-from .models import Product
+from .models import Product, APIRight
 
-admin.site.register(Product)
+# Enregistrement du modèle Product
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'price', 'description', 'created_at', 'updated_at')
+
+# Enregistrement du modèle APIRight
+@admin.register(APIRight)
+class APIRightAdmin(admin.ModelAdmin):
+    list_display = ('id', 'endpoint_name', 'token', 'can_access')
